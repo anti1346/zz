fd986883f5d4474da239436b818318be94424f24
Aby <42926329+anti1346@users.noreply.github.com>
2023-03-17 16:23:42 +0900

    Create jqinstall.sh

---
+++ jqinstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Check if jq is already installed
+if command -v jq &> /dev/null; then
+  echo "jq is already installed."
+else
+  # Check package manager and install jq
+  if command -v apt-get &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get install -y jq
+  elif command -v yum &> /dev/null; then
+    sudo yum update
+    sudo yum install -y jq
+  else
+    echo "Unable to determine package manager. Please install jq manually."
+    exit 1
+  fi
+fi
