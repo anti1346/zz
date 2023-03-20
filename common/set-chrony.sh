23c81f1c8456793e36afc928a7e86b0a597e820f
Aby <anti1346@gmail.com>
2023-03-20 12:10:09 +0900

    tr -d '"' 명령 추가

--- common/set-chrony.sh
+++ common/set-chrony.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # 운영체제 확인
-if [[ $(cat /etc/os-release | grep "^ID=" | awk -F'=' '{print $2}') == "ubuntu" ]]; then
+if [[ $(cat /etc/os-release | grep "^ID=" | awk -F'=' '{print $2}' | tr -d '"') == "ubuntu" ]]; then
     PACKAGE_MANAGER="apt-get"
     SERVICE_NAME="chrony.service"
     CONFIG_FILE_PATH="/etc/chrony/chrony.conf"
     sudo $PACKAGE_MANAGER update
-elif [[ $(cat /etc/os-release | grep "^ID=" | awk -F'=' '{print $2}') == "centos" ]]; then
+elif [[ $(cat /etc/os-release | grep "^ID=" | awk -F'=' '{print $2}' | tr -d '"') == "centos" ]]; then
     PACKAGE_MANAGER="yum"
     SERVICE_NAME="chronyd.service"
     CONFIG_FILE_PATH="/etc/chrony.conf"
@@ -19,33 +19,33 @@ else
     exit 1
 fi
 
-# # chrony 설치
-# sudo $PACKAGE_MANAGER install chrony -y
+# chrony 설치
+sudo $PACKAGE_MANAGER install chrony -y
 
-# # 기본 설정 파일 백업
-# sudo cp $CONFIG_FILE_PATH $CONFIG_FILE_PATH.bak
+# 기본 설정 파일 백업
+sudo cp $CONFIG_FILE_PATH $CONFIG_FILE_PATH.bak
 
-# # 서버 설정 추가
-# cat <<EOF > $CONFIG_FILE_PATH
-# server 169.254.169.123 iburst
-# server time.bora.net iburst
-# server times.postech.ac.kr iburst
+# 서버 설정 추가
+cat <<EOF > $CONFIG_FILE_PATH
+server 169.254.169.123 iburst
+server time.bora.net iburst
+server times.postech.ac.kr iburst
 
-# driftfile /var/lib/chrony/drift
+driftfile /var/lib/chrony/drift
 
-# makestep 1.0 3
+makestep 1.0 3
 
-# rtcsync
+rtcsync
 
-# logdir /var/log/chrony
+logdir /var/log/chrony
 
-# EOF
+EOF
 
-# # chrony 서비스 재시작
-# sudo systemctl restart $SERVICE_NAME
+# chrony 서비스 재시작
+sudo systemctl restart $SERVICE_NAME
 
-# chronyc sourcestats -v
+chronyc sourcestats -v
 
-# chronyc sources -v
+chronyc sources -v
 
-# chronyc tracking
+chronyc tracking
