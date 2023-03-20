a34730bde2e8b0b230d1970b154021d3899b3f04
Aby <anti1346@gmail.com>
2023-03-20 13:48:13 +0900

    no message

--- aws/script/ec2-hostname-change-init.sh
+++ aws/script/ec2-hostname-change-init.sh
@@ -34,6 +34,9 @@ else
     sudo aws --version
 fi
 
+cat <<'EOF' > ec2-hostname-change.sh
+#!/bin/bash
+
 instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
 
@@ -45,3 +48,18 @@ hostname=$(aws --region ${region} ec2 describe-instances \
 sudo hostnamectl set-hostname ${hostname}
 
 echo -e "\nHostname set to ${hostname}\n"
+EOF
+
+chmod +x ec2-hostname-change.sh
+
+# instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+# region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+
+# hostname=$(aws --region ${region} ec2 describe-instances \
+#     --instance-ids ${instance_id} \
+#     --query "Reservations[].Instances[].Tags[?Key=='Name'].Value[]" \
+#     --output text)
+
+# sudo hostnamectl set-hostname ${hostname}
+
+# echo -e "\nHostname set to ${hostname}\n"
