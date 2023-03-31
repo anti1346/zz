5dc98d09f2991434475f377e6a62e93daca5b67c
Aby <anti1346@gmail.com>
2023-03-31 13:55:35 +0900

    no message

--- common/install-tomcat-multiple.sh
+++ common/install-tomcat-multiple.sh
@@ -29,12 +29,25 @@ echo "Downloading and extracting Tomcat ${tomcat_version}..."
 wget -q "${tomcat_url}" -P /tmp/
 tar -xzf "/tmp/${tomcat_file}" -C "${initial_tomcat}" --strip-components=1
 
+# Array to store used ports
+used_ports=()
+
 # Loop to create Tomcat instances
+echo "Creating Tomcat instances..."
 echo "Creating Tomcat instances..."
 for i in {1..3}; do
     # Set Tomcat instance directory and port
     instance_dir="${tomcat_home}/instance${i}"
-    http_port=$(( 8080 + i - 1 ))
+    http_port=$(( 18080 + (i - 1) * 100 ))
+    shutdown_port=$(( http_port - 75 ))
+    ajp_port=$(( http_port + 1 ))
+    https_port=$(( http_port + 443 ))
+
+    # Check if the port is already in use
+    if lsof -Pi :$http_port -sTCP:LISTEN -t >/dev/null; then
+        echo "Port ${http_port} is already in use. Exiting."
+        exit 1
+    fi
 
     # Create Tomcat instance
     cp -r "${initial_tomcat}" "${instance_dir}"
@@ -43,13 +56,12 @@ for i in {1..3}; do
     echo "Configuring Tomcat instance ${i}..."
     cp "${initial_tomcat}/conf/server.xml" "${instance_dir}/conf/server.xml.bk"
     sed -i "s/8080/${http_port}/g" "${instance_dir}/conf/server.xml"
-    sed -i "s/8005/$(($http_port - 1))/g" "${instance_dir}/conf/server.xml"
-    sed -i "s/8009/$(($http_port + 1))/g" "${instance_dir}/conf/server.xml"
-    sed -i "s/8443/$(($http_port + 443))/g" "${instance_dir}/conf/server.xml"
+    sed -i "s/8005/${shutdown_port}/g" "${instance_dir}/conf/server.xml"
+    sed -i "s/8009/${ajp_port}/g" "${instance_dir}/conf/server.xml"
+    sed -i "s/8443/${https_port}/g" "${instance_dir}/conf/server.xml"
 
-    # Add a new web app (optional)
-    if ! [ -f "${instance_dir}/webapps/ROOT/test.jsp" ]; then
-        cat <<'EOT' > "${instance_dir}/webapps/ROOT/test.jsp"
+    if ! [ -f "${tomcat_home}/instance${i}/webapps/ROOT/test.jsp" ]; then
+        cat <<'EOT' > "${tomcat_home}/instance${i}/webapps/ROOT/test.jsp"
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ page import="java.lang.management.ManagementFactory" %>
@@ -95,10 +107,9 @@ EOT
     echo "Starting Tomcat instance ${i}..."
     "${instance_dir}/bin/startup.sh"
 
-    done
+done
 
 # Clean up
-rm -f /tmp/${tomcat_file}
+#rm -f /tmp/${tomcat_file}
 
 echo "Tomcat instances created successfully."
-
