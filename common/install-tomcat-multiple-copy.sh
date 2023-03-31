c3397f5b23a55efec8d68f882f8aed898760a77f
Aby <anti1346@gmail.com>
2023-03-31 13:29:04 +0900

    no message

--- common/install-tomcat-multiple-copy.sh
+++ common/install-tomcat-multiple-copy.sh
@@ -60,6 +60,40 @@ for i in {1..3}; do
     sed -i "s/8009/${ajp_port}/g" "${instance_dir}/conf/server.xml"
     sed -i "s/8443/${https_port}/g" "${instance_dir}/conf/server.xml"
 
+    if ! [ -f "${tomcat_home}/instance1/webapps/ROOT/test.jsp" ]; then
+        cat <<'EOT' > "${tomcat_home}/instance1/webapps/ROOT/test.jsp"
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="java.lang.management.ManagementFactory" %>
+<%@ page import="javax.management.MBeanServer" %>
+<%@ page import="javax.management.ObjectName" %>
+<%@ page import="org.apache.catalina.Server" %>
+<%@ page import="org.apache.catalina.util.ServerInfo" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>JSP Test Page</title>
+</head>
+<body>
+    <h1>JSP Test Page</h1>
+    <p>시간: <%= new java.util.Date() %></p>
+    <p>서버 이름: <%= request.getLocalName() %></p>
+    <p>서버 아이피: <%= request.getServerName() %></p>
+    <p>클라이언트 아이피: <%= request.getRemoteAddr() %></p>
+    <%
+    MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+    ObjectName name = new ObjectName("Catalina:type=Server");
+    Server server = (Server) mbs.getAttribute(name, "managedResource");
+    out.println("<p>인스턴스 정보: " + ServerInfo.getServerInfo() + "</p>");
+    out.println("<p>인스턴스 디렉토리 경로: " + server.getCatalinaBase().getAbsolutePath() + "</p>");
+    %>
+    <p>인스턴스 세션 ID: <%= session.getId() %></p>
+</body>
+</html>
+EOT
+    fi
+
     # Set Tomcat permissions
     echo "Setting Tomcat permissions..."
     chown -R tomcat: "${instance_dir}"
