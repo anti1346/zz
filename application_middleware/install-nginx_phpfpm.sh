cb09adb919c92785567f355eb446753aefd07ebc
aby <anti1346@gmail.com>
2023-04-10 22:01:56 +0900

    no message

--- application_middleware/install-nginx_phpfpm.sh
+++ application_middleware/install-nginx_phpfpm.sh
@@ -1,54 +1,67 @@
 #!/bin/bash
 
-# Check if running as root
+### 이 스크립트에서 발생한 에러가 무시되지 않도록 합니다.
+set -euo pipefail
+
+### Check if running as root
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root"
    exit 1
 fi
 
-# Check if running on Ubuntu or CentOS
+### Check if running on Ubuntu or CentOS
 if [[ -x "$(command -v apt-get)" ]]; then
     OS="Ubuntu"
 elif [[ -x "$(command -v yum)" ]]; then
     OS="CentOS"
 else
-    echo "Unsupported operating system"
+    echo "지원되지 않는 운영 체제입니다."
     exit 1
 fi
 
-# Update package lists
+### 패키지 리스트 업데이트
 if [[ $OS == "Ubuntu" ]]; then
-    sudo apt-get install -y ubuntu-keyring
-    sudo add-apt-repository ppa:ondrej/php
-    apt-get update -y
-    echo "127.0.0.1 $HOSTNAME" >> /etc/hosts
+    apt-get update
+    echo "127.0.0.1 $HOSTNAME" | sudo tee -a /etc/hosts >/dev/null
 elif [[ $OS == "CentOS" ]]; then
-    yum install -y epel-release yum-utils
+    sudo yum install -y epel-release yum-utils
 fi
 
-# Install nginx packages
+############################################################################################################
+############################################################################################################
+############################################################################################################
+### Nginx 패키지 설치
 if [[ $OS == "Ubuntu" ]]; then
-    # Import nginx signing key
+    NGINX_NGINXCONF="/etc/nginx/nginx.conf"
+    NGINX_DEFAULTCONF="/etc/nginx/conf.d/default.conf"
+
+    ### 필요한 종속성 설치
+    sudo apt-get install -y ubuntu-keyring
+        
+    ### Nginx 서명 키 가져오기
     curl -s https://nginx.org/keys/nginx_signing.key | gpg --dearmor | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
-    ### Verify the fingerprint. If it's different, remove the file.
+    ### 키 지문 확인. 다르면 파일 삭제.
     # gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
 
-    # Set up apt repository for stable nginx packages
+    ### 안정 버전 Nginx 패키지를 위한 apt 저장소 설정
     echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \
         | sudo tee /etc/apt/sources.list.d/nginx.list
 
-    # Update the package list and install Nginx
+    ### 패키지 리스트 업데이트 및 Nginx 설치
     sudo apt-get update
     sudo apt-get install -y nginx 
 elif [[ $OS == "CentOS" ]]; then
-    # Add the Nginx signing key
+    NGINX_NGINXCONF="/etc/nginx/nginx.conf"
+    NGINX_DEFAULTCONF="/etc/nginx/conf.d/default.conf"
+
+    ### Nginx 서명 키 추가
     sudo rpm --import https://nginx.org/keys/nginx_signing.key
 
-    # Add the Nginx repository
+    ### Nginx 저장소 추가
     sudo tee /etc/yum.repos.d/nginx.repo << EOF
 [nginx-stable]
 name=nginx stable repo
-baseurl=https://nginx.org/packages/centos/$releasever/$basearch/
+baseurl=https://nginx.org/packages/centos/\$releasever/\$basearch/
 gpgcheck=1
 enabled=1
 gpgkey=https://nginx.org/keys/nginx_signing.key
@@ -56,40 +69,44 @@ module_hotfixes=true
 
 [nginx-mainline]
 name=nginx mainline repo
-baseurl=https://nginx.org/packages/mainline/centos/$releasever/$basearch/
+baseurl=https://nginx.org/packages/mainline/centos/\$releasever/\$basearch/
 gpgcheck=1
-enabled=1
+enabled=0
 gpgkey=https://nginx.org/keys/nginx_signing.key
 module_hotfixes=true
 EOF
 
-    # Update the package list and install Nginx
-    sudo yum update
+    ### Nginx 설치
     sudo yum install -y nginx
 fi
 
-# Install php-fpm packages
+############################################################################################################
+############################################################################################################
+############################################################################################################
+# Install PHP-FPM packages
 if [[ $OS == "Ubuntu" ]]; then
-    PHP_VERSIOIN="php8.2"
-    sudo apt-get install -y $PHP_VERSIOIN $PHP_VERSIOIN-dev $PHP_VERSIOIN-cli $PHP_VERSIOIN-fpm $PHP_VERSIOIN-common $PHP_VERSIOIN-igbinary
-    sudo apt-get install -y $PHP_VERSIOIN-gd $PHP_VERSIOIN-mysql $PHP_VERSIOIN-curl $PHP_VERSIOIN-mbstring $PHP_VERSIOIN-mcrypt \
-        $PHP_VERSIOIN-intl $PHP_VERSIOIN-xml $PHP_VERSIOIN-redis $PHP_VERSIOIN-readline $PHP_VERSIOIN-mongodb $PHP_VERSIOIN-zip \
-        $PHP_VERSIOIN-imagick $PHP_VERSIOIN-rdkafka \ 
+    # Configure PHP-FPM
+    PHP_VERSIOIN="8.2"
+    PHPFPM_PHPINI="/etc/php/$PHP_VERSIOIN/fpm/php.ini"
+    PHPFPM_PHPFPMCONF="/etc/php/$PHP_VERSIOIN/fpm/php-fpm.conf"
+    PHPFPM_WWWCONF="/etc/php/$PHP_VERSIOIN/fpm/pool.d/www.conf"
+
+    sudo add-apt-repository ppa:ondrej/php
+    apt-get update -y
+
+    sudo apt-get install -y php$PHP_VERSIOIN php$PHP_VERSIOIN-dev php$PHP_VERSIOIN-cli php$PHP_VERSIOIN-fpm \
+        php$PHP_VERSIOIN-common php$PHP_VERSIOIN-igbinary
+
+    sudo apt-get install -y php$PHP_VERSIOIN-gd php$PHP_VERSIOIN-mysql php$PHP_VERSIOIN-curl php$PHP_VERSIOIN-mbstring \
+        php$PHP_VERSIOIN-mcrypt php$PHP_VERSIOIN-intl php$PHP_VERSIOIN-xml php$PHP_VERSIOIN-redis php$PHP_VERSIOIN-readline \
+        php$PHP_VERSIOIN-mongodb php$PHP_VERSIOIN-zip php$PHP_VERSIOIN-imagick php$PHP_VERSIOIN-rdkafka \
         php-json php-pear
 elif [[ $OS == "CentOS" ]]; then
+    # Configure Nginx
+    WWWCONF="/etc/php-fpm.d/www.conf"
     yum install -y php8.2-fpm
 fi
 
-# Configure Nginx, PHP-FPM
-if [[ $OS == "Ubuntu" ]]; then
-    NGINX_NGINXCONF="/etc/nginx/nginx.conf"
-    NGINX_DEFAULTCONF="/etc/nginx/conf.d/default.conf"
-    PHPFPM_PHPINI="/etc/php/8.2/fpm/php.ini"
-    PHPFPM_PHPFPMCONF="/etc/php/8.2/fpm/php-fpm.conf"
-    PHPFPM_WWWCONF="/etc/php/8.2/fpm/pool.d/www.conf"
-elif [[ $OS == "CentOS" ]]; then
-    WWWCONF="/etc/php-fpm.d/www.conf"
-fi
 
 # Configure Nginx
 sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.backup
