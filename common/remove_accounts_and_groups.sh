e4183c522f094de3afb9aeb11f4b2b7c25aec001
Aby <anti1346@gmail.com>
2023-04-24 10:09:49 +0900

    remove_accounts_and_groups.sh

---
+++ common/remove_accounts_and_groups.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# 삭제할 계정 목록
+REMOVE_ACCOUNT_LIST="games lp news uucp gnats"
+
+for account in $REMOVE_ACCOUNT_LIST; do
+    # 계정이 존재하는지 확인
+    if id -u $account >/dev/null 2>&1; then
+        # 계정 삭제
+        userdel -r $account
+        echo "Account $account has been removed."
+    else
+        echo "Account $account does not exist."
+    fi
+done
+
+# 삭제할 그룹 목록
+REMOVE_GROUP_LIST="games lp news uucp gnats"
+
+for group in $REMOVE_GROUP_LIST; do
+    # 그룹이 존재하는지 확인
+    if grep -q $group /etc/group; then
+        # 그룹 삭제
+        groupdel $group
+        echo "Group $group has been removed."
+    else
+        echo "Group $group does not exist."
+    fi
+done
