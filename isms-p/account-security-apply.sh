2a8aba293add2a834c2668a09daf62282e10f8e2
Aby <anti1346@gmail.com>
2023-05-09 14:48:17 +0900

    no message

--- isms-p/account-security-apply.sh
+++ isms-p/account-security-apply.sh
@@ -5,13 +5,7 @@ PW_MIN_LENGTH=8
 PW_MIN_CLASSES=3
 
 # Backup Settings
-BACKUP_DIR="/var/backups/passwd"
-BACKUP_FILE="passwd.bak.$(date +%Y%m%d-%H%M%S)"
-
-sudo sed -i 's/PASS_MAX_DAYS\s*99999/PASS_MAX_DAYS\t90/g' /etc/login.defs
-sudo sed -i 's/PASS_MIN_DAYS\s*0/PASS_MIN_DAYS\t1/g' /etc/login.defs
-sudo sed -i 's/PASS_WARN_AGE\s*7/PASS_WARN_AGE\t7/g' /etc/login.defs
-sudo sed -i 's/#PASS_MIN_LEN/PASS_MIN_LEN\t8/g' /etc/login.defs
+BACKUP_DATE="$(date +%Y%m%d-%H%M%S)"
 
 # Determine Linux Distro and Install Required Packages
 if [[ -f /etc/centos-release ]]; then
@@ -28,18 +22,45 @@ else
     exit 1
 fi
 
+### login.defs 파일 수정
+sudo cp /etc/login.defs /etc/login.defs.$BACKUP_DATE
+sudo sed -i 's/PASS_MAX_DAYS\s*99999/PASS_MAX_DAYS\t90/g' /etc/login.defs
+sudo sed -i 's/PASS_MIN_DAYS\s*0/PASS_MIN_DAYS\t1/g' /etc/login.defs
+sudo sed -i 's/PASS_WARN_AGE\s*7/PASS_WARN_AGE\t7/g' /etc/login.defs
+sudo sed -i 's/#PASS_MIN_LEN/PASS_MIN_LEN\t8/g' /etc/login.defs
+
 # Apply Password Policy Settings
 if [[ $DISTRO == "CentOS" || $DISTRO == "Amazon Linux" ]]; then
-    sed -i "s/^password.*pam_unix.so.*$/password    requisite     pam_pwquality.so minlen=$PW_MIN_LENGTH minclass=$PW_MIN_CLASSES retry=3/" /etc/pam.d/system-auth
+    sudo cp /etc/pam.d/system-auth /etc/pam.d/system-auth.$BACKUP_DATE
+    sudo cp /etc/pam.d/password-auth /etc/pam.d/password-auth.$BACKUP_DATE
+
+    if grep -Fxq "auth        required        pam_faillock.so preauth silent audit deny=5 unlock_time=600" /etc/pam.d/system-auth
+    then
+        echo "pam_faillock already configured in system-auth file"
+    else
+        sed -i '/auth.*required.*pam_env.so/a auth        required        pam_faillock.so preauth silent audit deny=5 unlock_time=600' /etc/pam.d/system-auth
+        echo "pam_faillock module added to system-auth file"
+    fi
+
+    # Check if the pam_faillock module is already configured in password-auth file
+    if grep -Fxq "auth        required        pam_faillock.so preauth silent audit deny=5 unlock_time=600" /etc/pam.d/password-auth
+    then
+        echo "pam_faillock already configured in password-auth file"
+    else
+        sed -i '/auth.*required.*pam_env.so/a auth        required        pam_faillock.so preauth silent audit deny=5 unlock_time=600' /etc/pam.d/password-auth
+        echo "pam_faillock module added to password-auth file"
+    fi
 elif [[ $DISTRO == "Ubuntu" ]]; then
-    sed -i "s/^password.*pam_unix.so.*$/password    requisite     pam_pwquality.so minlen=$PW_MIN_LENGTH minclass=$PW_MIN_CLASSES retry=3/" /etc/pam.d/common-password
+    sudo cp /etc/pam.d/common-password /etc/pam.d/common-password.$BACKUP_DATE
+
+    if grep -Fxq "password.*requisite.*pam_pwquality.so" /etc/pam.d/common-password
+    then
+        echo "pam_pwquality already configured in common-password file"
+    else
+        sed -i "s/^password.*pam_unix.so.*$/password    requisite     pam_pwquality.so minlen=8 minclass=3 dcredit=-1 lcredit=-1 ocredit=-1 retry=3/" /etc/pam.d/common-password
+        echo "pam_pwquality module added to common-password file"
+    fi
 fi
 
-# Backup /etc/passwd and /etc/shadow files
-mkdir -p $BACKUP_DIR
-cp -p /etc/passwd $BACKUP_DIR/$BACKUP_FILE
-cp -p /etc/shadow $BACKUP_DIR/$BACKUP_FILE
-
-echo "Password policy applied and backup files created."
 
 
