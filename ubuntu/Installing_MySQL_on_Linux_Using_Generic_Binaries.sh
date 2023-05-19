90819adb38cea59f3d7d1f40ffc527f9ce450d4a
Aby <42926329+anti1346@users.noreply.github.com>
2023-05-19 22:55:58 +0900

    Update Installing_MySQL_on_Linux_Using_Generic_Binaries.sh

--- ubuntu/Installing_MySQL_on_Linux_Using_Generic_Binaries.sh
+++ ubuntu/Installing_MySQL_on_Linux_Using_Generic_Binaries.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
+### Define MySQL URL
 #mysql_url="https://downloads.mysql.com/archives/get/p/23/file/mysql-5.7.41-linux-glibc2.12-x86_64.tar"
 mysql_url="https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-8.0.33-linux-glibc2.17-aarch64.tar"
 
+
+### Define MySQL installation details
 mysql_basedir=/usr/local/mysql
 mysql_username=mysql
 mysql_groupname=mysql
 mysql_userid=121
 mysql_groupid=121
 
+### Extract MySQL filename and version from the URL
 mysql_filename=$(basename "$mysql_url")
 mysql_version=$(echo "$mysql_filename" | grep -oP '\d+\.\d+\.\d+')
 
+### Check if required packages are installed, and install if not
 if ! dpkg -s libaio1 >/dev/null 2>&1; then
   sudo apt-get update
   sudo apt-get install -qq -y libaio1 libnuma1 libncurses5
 fi
 
-# Check if MySQL user exists
+### Check if MySQL user exists and create if not
 if ! id -u ${mysql_username} >/dev/null 2>&1; then
   sudo groupadd --gid ${mysql_groupid} ${mysql_groupname}
   sudo useradd -r --uid ${mysql_userid} -g ${mysql_groupname} -s /bin/false ${mysql_username}
 fi
 
+### Set the MySQL base directory based on version
 if [ ! -d "${mysql_basedir}" ]; then
   base_dir=${mysql_basedir}
 elif [ ! -d "${mysql_basedir}-${mysql_version}" ]; then
@@ -31,10 +37,12 @@ else
   exit 127
 fi
 
+### Set data directory and user/group names
 data_dir=${base_dir}/data
 user_name=${mysql_username}
 group_name=${mysql_groupname}
 
+### Create MySQL base directory if it doesn't exist
 if [ ! -d "${base_dir}" ]; then
   mkdir "${base_dir}"
 else
@@ -43,11 +51,12 @@ fi
 
 cd /tmp
 
-# Download MySQL archive
+### Download MySQL
 wget -q ${mysql_url}
 
 tar xf ${mysql_filename}
 
+### Create data directory if it doesn't exist
 if [ ! -d "${data_dir}" ]; then
   mkdir "${data_dir}"
 else
@@ -78,11 +87,14 @@ EOF
 
 cd ${base_dir}
 
+### Initialize MySQL
 # /usr/local/mysql/bin/mysqld --initialize-insecure --user=mysql --basedir=/usr/local/mysql --datadir=/usr/local/mysql/data
 ${base_dir}/bin/mysqld --initialize-insecure --user=${user_name} --basedir=${base_dir} --datadir=${data_dir}
 
+### Start MySQL
 # /usr/local/mysql/bin/mysqld --defaults-file=/usr/local/mysql/my.cnf --user=mysql &
 ${base_dir}/bin/mysqld_safe --defaults-file=${base_dir}/my.cnf --user=${user_name} &
 
+### Uncomment the following lines if you want to connect to MySQL using the command-line client and then shutdown the server
 # /usr/local/mysql/bin/mysql -uroot --socket /usr/local/mysql/mysql.sock
 # /usr/local/mysql/bin/mysqladmin -u root shutdown --socket /usr/local/mysql/mysql.sock
