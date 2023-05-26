176c4ba7fa4cb7d6b916c9caf01554d11dc3d304
aby <anti1346@gmail.com>
2023-05-26 22:49:56 +0900

    no message

--- common/install-docker.sh
+++ common/install-docker.sh
@@ -1,52 +1,63 @@
 #!/bin/bash
 
+# 운영체제 정보 확인
 distro=$(lsb_release -i | cut -f2)
 os_version=$(lsb_release -sr | cut -d'.' -f1)
 
 # 도커 설치
-if [ "$distro" == "CentOS" ]; then
-    if [[ $os_version -eq 8 || $os_version -eq 7 ]]; then
-        echo "CentOS $os_version"
+if ! command -v docker >/dev/null; then
+    if [ "$distro" == "CentOS" ]; then
+        if [[ $os_version -eq 8 || $os_version -eq 7 ]]; then
+            echo "CentOS $os_version"
+            curl -fsSL https://get.docker.com -o get-docker.sh
+            chmod +x get-docker.sh
+            bash get-docker.sh
+            usermod -aG docker $(whoami)
+            systemctl --now enable docker.service
+        fi
+    elif [ "$distro" == "Ubuntu" ]; then
+        echo "Ubuntu $os_version"
         curl -fsSL https://get.docker.com -o get-docker.sh
         chmod +x get-docker.sh
-        bash get-docker.sh
+        sudo bash get-docker.sh
         usermod -aG docker $(whoami)
         systemctl --now enable docker.service
+    elif [ "$distro" == "Amazon" ]; then
+        echo "Amazon $os_version"
+        amazon-linux-extras install -y epel
+        amazon-linux-extras install -y docker
+        usermod -aG docker ec2-user
+        systemctl --now enable docker.service
+    else
+        echo "Other OS"
     fi
-elif [ "$distro" == "Ubuntu" ]; then
-    echo "Ubuntu $os_version"
-    curl -fsSL https://get.docker.com -o get-docker.sh
-    chmod +x get-docker.sh
-    sudo bash get-docker.sh
-    usermod -aG docker $(whoami)
-    systemctl --now enable docker.service
-elif [ "$distro" == "Amazon" ]; then
-    echo "Amazon $os_version"
-    amazon-linux-extras install -y epel
-    amazon-linux-extras install -y docker
-    usermod -aG docker ec2-user
-    systemctl --now enable docker.service
 else
-    echo "Other OS"
+    echo "Docker already installed"
 fi
 
-# # 도커 컴포즈 설치
-# curl -fsSL "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# chmod +x /usr/local/bin/docker-compose
-# ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+# 도커 컴포즈 설치
+if ! command -v docker-compose >/dev/null; then
+    echo "Installing Docker Compose"
+    curl -fsSL "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+else
+    echo "Docker Compose already installed"
+fi
 
-# # CTOP 설치
-# CTOP=${CTOPVersion:-0.7.7}
-# #https://github.com/bcicen/ctop/releases/download/v0.7.7/ctop-0.7.7-linux-amd64
-# curl -fsSL https://github.com/bcicen/ctop/releases/download/v${CTOP}/ctop-${CTOP}-linux-amd64 -o /usr/local/bin/ctop
-# chmod +x /usr/local/bin/ctop
-# ln -s /usr/local/bin/ctop /usr/bin/ctop
+# CTOP 설치
+if ! command -v ctop >/dev/null; then
+    echo "Installing CTOP"
+    CTOP=${CTOPVersion:-0.7.7}
+    curl -fsSL https://github.com/bcicen/ctop/releases/download/v${CTOP}/ctop-${CTOP}-linux-amd64 -o /usr/local/bin/ctop
+    chmod +x /usr/local/bin/ctop
+    ln -s /usr/local/bin/ctop /usr/bin/ctop
+else
+    echo "CTOP already installed"
+fi
 
 # 스크립트 종료
 exit 0
-
-
-
 
 # lsb_release 명령으로 운영체제 판단
 # if command -v apt >/dev/null; then
