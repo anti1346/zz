5068d36d575f90b8703a85fb91fd6c2464ee7b95
aby <anti1346@gmail.com>
2023-05-26 21:51:49 +0900

    no message

--- common/install-docker.sh
+++ common/install-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-### lsb_release 명령으로 운영체제 판단 ###
+# lsb_release 명령으로 운영체제 판단
 if command -v apt >/dev/null; then
     echo "Linux Distribution : Debian"
     apt update -qq -y >/dev/null 2>&1
@@ -17,9 +17,7 @@ fi
 distro=$(lsb_release -i | cut -f2)
 os_version=$(lsb_release -sr | cut -d'.' -f1)
 
-
-
-### 도커 설치 ###
+# 도커 설치
 if [ "$distro" == "CentOS" ]; then
     if [[ $os_version -eq 8 || $os_version -eq 7 ]]; then
         echo "CentOS $os_version"
@@ -45,18 +43,17 @@ if [ "$distro" == "CentOS" ]; then
         echo "Other OS"
 fi
 
-
-
-### 도커 컴포즈 설치 ###
+# 도커 컴포즈 설치
 curl -fsSL "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
+# CTOP 설치
+CTOP=${CTOPVersion:-0.7.7}
+#https://github.com/bcicen/ctop/releases/download/v0.7.7/ctop-0.7.7-linux-amd64
+curl -fsSL https://github.com/bcicen/ctop/releases/download/v${CTOP}/ctop-${CTOP}-linux-amd64 -o /usr/local/bin/ctop
+chmod +x /usr/local/bin/ctop
+ln -s /usr/local/bin/ctop /usr/bin/ctop
 
-
-# ### CTOP 설치 ###
-# CTOP=${CTOPVersion:-0.7.7}
-# #https://github.com/bcicen/ctop/releases/download/v0.7.7/ctop-0.7.7-linux-amd64
-# curl -fsSL https://github.com/bcicen/ctop/releases/download/v${CTOP}/ctop-${CTOP}-linux-amd64 -o /usr/local/bin/ctop
-# chmod +x /usr/local/bin/ctop
-# ln -s /usr/local/bin/ctop /usr/bin/ctop
+# 스크립트 종료
+exit 0
