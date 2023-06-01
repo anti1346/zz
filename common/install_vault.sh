469ace31f36b15b59b57ef24276a2058bae828ee
Aby <42926329+anti1346@users.noreply.github.com>
2023-06-01 21:46:01 +0900

    Update install_vault.sh

--- common/install_vault.sh
+++ common/install_vault.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# 아키텍처 확인
+ARCH=$(uname -m)
+if [[ ${ARCH} == "x86_64" ]]; then
+  VAULT_ARCH="amd64"
+elif [[ ${ARCH} == "aarch64" ]]; then
+  VAULT_ARCH="arm64"
+else
+  echo "Unsupported architecture: ${ARCH}"
+  exit 1
+fi
+
 # Vault 버전 및 다운로드 URL 설정
 VAULT_VERSION="1.13.2"
-VAULT_ZIP="vault_${VAULT_VERSION}_linux_amd64.zip"
+VAULT_ZIP="vault_${VAULT_VERSION}_linux_${VAULT_ARCH}.zip"
 VAULT_URL="https://releases.hashicorp.com/vault/${VAULT_VERSION}/${VAULT_ZIP}"
 
 # Vault 다운로드 및 설치
@@ -10,5 +21,9 @@ curl -LO "${VAULT_URL}"
 unzip "${VAULT_ZIP}"
 sudo mv vault /usr/local/bin/
 
+# Vault 삭제
+rm -f "${VAULT_ZIP}"
+
 # 설치 확인
 vault --version
+
