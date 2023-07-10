a27f9e6299dda65c3d5e8da3d9cd207d704bb75f
aby <anti1346@gmail.com>
2023-07-10 20:20:13 +0900

    no message

--- common/myip.sh
+++ common/myip.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# 출력을 위한 색상 코드
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 # 내부 IP 가져오기
 INTERNAL_IP=$(hostname -I | awk '{print $1}')
-
 # 외부 IP 가져오기
-EXTERNAL_IP=$(curl -s https://checkip.amazonaws.com)
+EXTERNAL_IP=$(curl -s --max-time 3 https://checkip.amazonaws.com)
 
-echo "내부 IP: $INTERNAL_IP"
-echo "외부 IP: $EXTERNAL_IP"
+echo -e "${RED}내부 IP: $INTERNAL_IP${NC}"
+echo -e "${GREEN}외부 IP: $EXTERNAL_IP${NC}"
