c5c43b92880212026f65f0643d34c5daed07d6b0
Aby <42926329+anti1346@users.noreply.github.com>
2023-10-02 13:27:32 +0000

    Update al2023-nginx.sh

--- aws/al2023-nginx.sh
+++ aws/al2023-nginx.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+sudo yum install -y yum-utils
+
+cat <<'EOF' > /etc/yum.repos.d/nginx.repo
+[nginx-stable]
+name=nginx stable repo
+baseurl=http://nginx.org/packages/amzn/2023/$basearch/
+gpgcheck=1
+enabled=1
+gpgkey=https://nginx.org/keys/nginx_signing.key
+module_hotfixes=true
+priority=9
+
+[nginx-mainline]
+name=nginx mainline repo
+baseurl=http://nginx.org/packages/mainline/amzn/2023/$basearch/
+gpgcheck=1
+enabled=0
+gpgkey=https://nginx.org/keys/nginx_signing.key
+module_hotfixes=true
+priority=9
+EOF
+
+sudo yum-config-manager --enable nginx-stable
+
+sudo yum install -y nginx
+
+sudo systemctl --now enable nginx
