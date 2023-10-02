d61ec77d6d54c536046bd2e9db23b00051ad4b37
Aby <42926329+anti1346@users.noreply.github.com>
2023-10-02 14:48:06 +0000

    Update amzn2-nginx.sh

--- aws/amzn2-nginx.sh
+++ aws/amzn2-nginx.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 
-sudo amazon-linux-extras install -y epel-release
+###sudo yum install -y epel-release
+sudo amazon-linux-extras install -y epel
 
-sudo amazon-linux-extras install -y nginx1
+sudo yum install -y yum-utils
+
+sudo cat <<'EOF' > /etc/yum.repos.d/nginx.repo
+[nginx-stable]
+name=nginx stable repo
+baseurl=http://nginx.org/packages/amzn/2023/$basearch/
+gpgcheck=1
+enabled=1
+gpgkey=https://nginx.org/keys/nginx_signing.key
+module_hotfixes=true
+priority=9
+
+[nginx-mainline]
+name=nginx mainline repo
+baseurl=http://nginx.org/packages/mainline/amzn/2023/$basearch/
+gpgcheck=1
+enabled=0
+gpgkey=https://nginx.org/keys/nginx_signing.key
+module_hotfixes=true
+priority=9
+EOF
+
+sudo yum-config-manager --enable nginx-stable
+
+sudo yum install -y nginx
+
+sudo systemctl --now enable nginx
+
+echo "NGINT TEST PAGE" > /usr/share/nginx/html/tt.html
 
