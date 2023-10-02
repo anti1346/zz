a1d75c30293ae40920c475ee542c15fa7d00edb2
Aby <42926329+anti1346@users.noreply.github.com>
2023-10-02 14:24:04 +0000

    Update al2023-phpfpm.sh

--- aws/al2023-phpfpm.sh
+++ aws/al2023-phpfpm.sh
@@ -2,20 +2,26 @@
 
 sudo yum install -y epel-release
 
-sudo yum install -y php8.1 php8.1-cli php8.1-common php8.1-devel php8.1-fpm
-sudo yum install -y php8.1-bcmath php8.1-gd php8.1-intl php8.1-mbstring php8.1-mysqlnd php8.1-pdo php8.1-xml
-sudo yum install -y php-pear
+sudo yum install -y php8.1php8.1 php8.1-cli php8.1-common php8.1-devel php8.1-fpm
+sudo yum install -y php-gd php-mbstring php-xml php-intl php-mysqlnd
 
-sudo yum install -y  ImageMagick ImageMagick-devel
-sudo yum install -y re2c librdkafka librdkafka-devel
+sudo systemctl --now enable php-fpm
 
+#echo "<?php phpinfo(); ?>" > /usr/share/nginx/html/phpinfo.php
 
-echo | pecl install redis
-echo | pecl install mongodb
-echo | pecl install imagick
-echo | pecl install rdkafka
-echo | pecl install zip
-
-echo "extension=redis.so" >> /etc/php.ini
-echo "extension=mongodb.so" >> /etc/php.ini
-echo "extension=imagick.so" >> /etc/php.ini
+#sudo yum install -y php8.1 php8.1-cli php8.1-common php8.1-devel php8.1-fpm
+#sudo yum install -y php8.1-bcmath php8.1-gd php8.1-intl php8.1-mbstring php8.1-mysqlnd php8.1-pdo php8.1-xml
+#sudo yum install -y php-pear
+#
+#sudo yum install -y  ImageMagick ImageMagick-devel
+#sudo yum install -y re2c librdkafka librdkafka-devel
+#
+#echo | pecl install redis
+#echo | pecl install mongodb
+#echo | pecl install imagick
+#echo | pecl install rdkafka
+#echo | pecl install zip
+#
+#echo "extension=redis.so" >> /etc/php.ini
+#echo "extension=mongodb.so" >> /etc/php.ini
+#echo "extension=imagick.so" >> /etc/php.ini
