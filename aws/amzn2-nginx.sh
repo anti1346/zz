3b4c47857544569733af18f392fdd73a93ab6080
Aby <42926329+anti1346@users.noreply.github.com>
2023-10-04 14:02:58 +0900

    Update amzn2-nginx.sh

--- aws/amzn2-nginx.sh
+++ aws/amzn2-nginx.sh
@@ -10,34 +10,34 @@ done
 ###sudo yum install -y epel-release
 sudo amazon-linux-extras install -y epel
 
-sudo yum install -y yum-utils
+# sudo yum install -y yum-utils
 
-sudo cat <<'EOF' > /etc/yum.repos.d/nginx.repo
-[nginx-stable]
-name=nginx stable repo
-baseurl=https://nginx.org/packages/amzn/2023/$basearch/
-gpgcheck=1
-enabled=1
-gpgkey=https://nginx.org/keys/nginx_signing.key
-module_hotfixes=true
-priority=9
+# sudo cat <<'EOF' > /etc/yum.repos.d/nginx.repo
+# [nginx-stable]
+# name=nginx stable repo
+# baseurl=https://nginx.org/packages/amzn/2023/$basearch/
+# gpgcheck=1
+# enabled=1
+# gpgkey=https://nginx.org/keys/nginx_signing.key
+# module_hotfixes=true
+# priority=9
 
-[nginx-mainline]
-name=nginx mainline repo
-baseurl=https://nginx.org/packages/mainline/amzn/2023/$basearch/
-gpgcheck=1
-enabled=0
-gpgkey=https://nginx.org/keys/nginx_signing.key
-module_hotfixes=true
-priority=9
-EOF
+# [nginx-mainline]
+# name=nginx mainline repo
+# baseurl=https://nginx.org/packages/mainline/amzn/2023/$basearch/
+# gpgcheck=1
+# enabled=0
+# gpgkey=https://nginx.org/keys/nginx_signing.key
+# module_hotfixes=true
+# priority=9
+# EOF
 
-sudo yum-config-manager --enable nginx-stable
+# sudo yum-config-manager --enable nginx-stable
 
-sudo yum install -y libcrypt
+# sudo yum install -y libcrypt
 
-### sudo amazon-linux-extras install nginx1
-sudo yum install -y nginx
+# sudo yum install -y nginx
+sudo amazon-linux-extras install -y nginx1
 
 sudo systemctl --now enable nginx
 
