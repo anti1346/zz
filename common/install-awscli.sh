560cd76f6f8e6a86e44cdceca1276699f17f4d5d
Aby <42926329+anti1346@users.noreply.github.com>
2023-10-04 13:39:32 +0900

    Update awscli.sh

--- common/install-awscli.sh
+++ common/install-awscli.sh
@@ -1,39 +1,44 @@
 #!/bin/bash
 
-# Get aws-cli version
+# Get the installed aws-cli version
 aws_version=$(aws --version | awk '{print $1}' | awk -F'/' '{print $2}')
 
 # Check if aws-cli is already installed
 if command -v aws &>/dev/null; then
-    echo "aws is already installed."
-    echo "aws-cli version: $aws_version"
+    echo "AWS CLI is already installed."
+    echo "AWS CLI version: $aws_version"
     exit 0
 fi
 
-# Download and install aws-cli based on the operating system
+# Determine the operating system and install aws-cli accordingly
 if [[ "$(uname -a)" == *"amzn2"* ]]; then
-    # Amazon Linux 2
-    curl -fsSL "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    sudo unzip -q awscliv2.zip
-    sudo ./aws/install
+    # Install on Amazon Linux 2
+    os_name="Amazon Linux 2"
+    download_url="https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
+    home_dir="/home/ec2-user"
 elif [[ "$(uname -a)" == *"el7"* ]]; then
-    # CentOS 7
-    curl -fsSL "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    sudo unzip -q awscliv2.zip
-    sudo ./aws/install
+    # Install on CentOS 7
+    os_name="CentOS 7"
+    download_url="https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
+    home_dir="/root"
 elif [[ "$(uname -a)" == *"Ubuntu"* ]]; then
-    # Ubuntu
+    # Install on Ubuntu
+    os_name="Ubuntu"
     sudo apt-get update -qq
     sudo apt-get install -qq -y unzip
-    curl -fsSL "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    sudo unzip -q awscliv2.zip
-    sudo ./aws/install
+    download_url="https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
+    home_dir="/home/ubuntu"
 else
     echo "Unsupported operating system."
     exit 1
 fi
 
-echo "aws-cli version: $aws_version"
+# Download and install aws-cli
+curl -fsSL "$download_url" -o "$home_dir/awscliv2.zip"
+unzip -q "$home_dir/awscliv2.zip"
+sudo "$home_dir/aws/install"
+
+echo "AWS CLI version: $aws_version"
 
 # Clean up
-rm -rf awscliv2.zip
+rm -rf "$home_dir/awscliv2.zip" "$home_dir/aws"
