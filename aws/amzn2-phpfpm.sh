d345fc7f053a0d8936286444c059cfe18531fe7e
Aby <42926329+anti1346@users.noreply.github.com>
2023-10-04 11:26:50 +0900

    Update amzn2-phpfpm.sh

--- aws/amzn2-phpfpm.sh
+++ aws/amzn2-phpfpm.sh
@@ -3,6 +3,8 @@
 # Amazon Linux 2 PHP-FPM 8.1 설치 스크립트
 
 # 패키지 업데이트
+###sudo yum install -y epel-release
+sudo amazon-linux-extras install -y epel
 
 # PHP 저장소 추가 및 PHP 8.1 설치
 sudo amazon-linux-extras install -y php8.1
