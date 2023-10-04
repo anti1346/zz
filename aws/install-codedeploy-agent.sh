0b500acef70968c658d28960b4677571752cc4d4
Aby <42926329+anti1346@users.noreply.github.com>
2023-10-05 08:59:28 +0900

    Update install-codedeploy-agent.sh

--- aws/install-codedeploy-agent.sh
+++ aws/install-codedeploy-agent.sh
@@ -1,30 +1,50 @@
 #!/bin/bash
 
-# Stop and remove codedeploy-agent service
+# 리눅스 배포판 확인
+os_distribution=$(grep -oP '(?<=^PRETTY_NAME=")(.*)(?=")' /etc/os-release)
+
+case "$os_distribution" in
+  "Amazon Linux 2")
+    package_manager="yum"
+    ;;
+  "CentOS Linux"*)
+    package_manager="yum"
+    ;;
+  "Ubuntu"*)
+    package_manager="apt"
+    ;;
+  *)
+    echo "지원되지 않는 운영 체제 배포판"
+    exit 1
+    ;;
+esac
+
+# 필수 패키지 설치
+sudo $package_manager install -y curl jq
+
+# AWS CodeDeploy 에이전트 서비스 중지 및 제거
 sudo systemctl stop codedeploy-agent
 sudo systemctl disable codedeploy-agent
+sudo $package_manager erase -y codedeploy-agent
 
-# Uninstall codedeploy-agent
-sudo yum erase -qq -y codedeploy-agent
-
-# Remove existing codedeploy-agent files
+# 기존 AWS CodeDeploy 에이전트 파일 제거
 sudo rm -rf /opt/codedeploy-agent
 
-# Create codedeploy-agent directory
+# AWS CodeDeploy 에이전트 디렉토리 생성
 sudo mkdir -p /opt/codedeploy-agent
 
-# Download and install codedeploy-agent
+# AWS CodeDeploy 에이전트 다운로드 및 설치
 #wget -q https://aws-codedeploy-ap-northeast-2.s3.ap-northeast-2.amazonaws.com/latest/install -O install
 REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
 wget -q "https://aws-codedeploy-${REGION}.s3.${REGION}.amazonaws.com/latest/install" -O /tmp/install
 chmod +x /tmp/install
 sudo /tmp/install auto
 
-# Update codedeploy-agent
+# AWS CodeDeploy 에이전트 업데이트
 sudo /opt/codedeploy-agent/bin/install auto
 
-# Start and enable codedeploy-agent service
+# AWS CodeDeploy 에이전트 서비스 시작 및 활성화
 sudo systemctl --now enable codedeploy-agent
 
-# Check the status of codedeploy-agent
+# AWS CodeDeploy 에이전트 상태 확인
 sudo systemctl status codedeploy-agent
