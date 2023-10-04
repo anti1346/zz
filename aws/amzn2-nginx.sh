c8db5d10d617fe397cdc1364f9b3d499cf1d66d6
Aby <42926329+anti1346@users.noreply.github.com>
2023-10-04 13:59:46 +0900

    Update amzn2-nginx.sh

--- aws/amzn2-nginx.sh
+++ aws/amzn2-nginx.sh
@@ -15,7 +15,7 @@ sudo yum install -y yum-utils
 sudo cat <<'EOF' > /etc/yum.repos.d/nginx.repo
 [nginx-stable]
 name=nginx stable repo
-baseurl=http://nginx.org/packages/amzn/2023/$basearch/
+baseurl=https://nginx.org/packages/amzn/2023/$basearch/
 gpgcheck=1
 enabled=1
 gpgkey=https://nginx.org/keys/nginx_signing.key
@@ -24,7 +24,7 @@ priority=9
 
 [nginx-mainline]
 name=nginx mainline repo
-baseurl=http://nginx.org/packages/mainline/amzn/2023/$basearch/
+baseurl=https://nginx.org/packages/mainline/amzn/2023/$basearch/
 gpgcheck=1
 enabled=0
 gpgkey=https://nginx.org/keys/nginx_signing.key
