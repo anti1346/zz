3b00224d56574a3ea9d621c8b50b96ee01f24574
Aby <42926329+anti1346@users.noreply.github.com>
2023-10-05 09:10:01 +0900

    Update cloudwatch-agent.sh

--- aws/cloudwatch-agent.sh
+++ aws/cloudwatch-agent.sh
@@ -21,10 +21,10 @@ esac
 
 # CloudWatch 에이전트 설치
 if [ "$package_manager" = "yum" ]; then
-  rpm -Uvh https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
+  sudo rpm -Uvh https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm
 elif [ "$package_manager" = "apt" ]; then
   wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
-  dpkg -i amazon-cloudwatch-agent.deb
+  sudo dpkg -i amazon-cloudwatch-agent.deb
 fi
 
 # CloudWatch 에이전트 구성
@@ -57,5 +57,8 @@ EOF
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json -s
 
 # CloudWatch 에이전트 서비스 시작 및 활성화
-systemctl start amazon-cloudwatch-agent
-systemctl enable amazon-cloudwatch-agent
+sudo systemctl start amazon-cloudwatch-agent
+sudo systemctl enable amazon-cloudwatch-agent
+
+# CloudWatch 에이전트 서비스 재시작
+sudo systemctl restart amazon-cloudwatch-agent
