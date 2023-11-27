fc5adde3acaccb6337fc9e3262685d3098e019c3
Aby <42926329+anti1346@users.noreply.github.com>
2023-11-27 22:27:09 +0900

    Create linux-utilization.sh

---
+++ etc/linux-utilization.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+while true; do
+    # 현재 시간을 가져옴
+    current_time=$(date +"%Y-%m-%d %H:%M:%S")
+
+    # CPU 사용량 확인
+    cpu_usage=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
+
+    # 메모리 사용량 확인
+    total_memory=$(free -m | awk '/^Mem:/ {print $2}')
+    used_memory=$(free -m | awk '/^Mem:/ {print $3}')
+    memory_usage=$(awk "BEGIN {printf \"%.2f\", $used_memory/$total_memory*100}")
+
+    # Disk I/O 사용량 확인
+    disk_io=$(iostat -d -x 1 1 | awk '/^sda/ {print "Disk I/O: " $14 "%"}')
+
+    # 결과 출력
+    printf "%s | CPU utilization : %.2f%% | Memory utilization : %.2f%% | %s\n" "$current_time" "$cpu_usage" "$memory_usage" "$disk_io"
+
+    # 1초 대기
+    sleep 1
+done
