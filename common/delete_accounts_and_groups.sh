c63cadeb29a3859b9bce447b146cbc6f3016141d
Aby <42926329+anti1346@users.noreply.github.com>
2023-12-12 11:27:22 +0900

    Update and rename remove_accounts_and_groups.sh to delete_accounts_and_groups.sh

--- common/delete_accounts_and_groups.sh
+++ common/delete_accounts_and_groups.sh
@@ -5,14 +5,16 @@
 ### 그룹 목록 출력
 # cut -d: -f1 /etc/group
 
+#!/bin/bash
+
 # 삭제할 계정 목록
 REMOVE_ACCOUNT_LIST="games lp news uucp gnats sync proxy backup list irc pollinate landscape fwupd-refresh lxd uuidd tss systemd-resolve mail"
 
 for account in $REMOVE_ACCOUNT_LIST; do
     # 계정이 존재하는지 확인
-    if id -u $account >/dev/null 2>&1; then
+    if id -u "$account" &>/dev/null; then
         # 계정 삭제
-        userdel -r $account
+        userdel -r "$account"
         echo "Account $account has been removed."
     else
         echo "Account $account does not exist."
@@ -24,11 +26,13 @@ REMOVE_GROUP_LIST="games lp news uucp gnats fax voice floppy tape audio video sy
 
 for group in $REMOVE_GROUP_LIST; do
     # 그룹이 존재하는지 확인
-    if grep -q $group /etc/group; then
+    if getent group "$group" &>/dev/null; then
         # 그룹 삭제
-        groupdel $group
+        groupdel "$group"
         echo "Group $group has been removed."
     else
         echo "Group $group does not exist."
     fi
 done
+
+echo "Script execution completed."
