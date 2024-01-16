627663d2f6d705683a7768e778881357eef490af
Aby <42926329+anti1346@users.noreply.github.com>
2024-01-17 08:31:28 +0900

    Update install_ssh_server.sh

--- etc/install_ssh_server.sh
+++ etc/install_ssh_server.sh
@@ -2,6 +2,7 @@
 
 if command -v apt-get &>/dev/null; then
     package_manager=apt-get
+    sudo ${package_manager} update
 elif command -v yum &>/dev/null; then
     package_manager=yum
 else
@@ -9,8 +10,8 @@ else
     exit 1
 fi
 
-sudo ${package_manager} update
 sudo ${package_manager} install -y openssh-server
+
 sudo systemctl start sshd
 
 echo "OpenSSH server installed and started successfully."
