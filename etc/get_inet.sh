9455a8637a54afd213f7d33ecdb9d25545d1cf15
Aby <42926329+anti1346@users.noreply.github.com>
2024-01-23 12:31:14 +0900

    Update get_inet.sh

--- etc/get_inet.sh
+++ etc/get_inet.sh
@@ -2,4 +2,6 @@
 
 ifconfig | awk '/^[a-z]/{gsub(/:/, "", $1); dev=$1; next} /inet[^6]/ && $2 !~ /127.0.0.1/ && dev != "docker0" {print dev}'
 
-#
+
+### Shell Execute Command
+# curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/get_inet.sh | bash
