bca8cdc351459301e0610c8ff7aa377ba85eae9f
Aby <42926329+anti1346@users.noreply.github.com>
2024-01-24 14:46:15 +0900

    Update set_proxy_server.sh

--- etc/set_proxy_server.sh
+++ etc/set_proxy_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 제공된 인수 또는 기본값을 사용하여 프록시 서버 및 포트를 설정합니다.
-proxy_server="${1:-192.168.0.100:8080}"
+proxy_server="${1:-ProxyServerIP:3128}"
 proxy_path="${2:-~/.bashrc}"
 
 # 지정된 파일에 프록시 변수가 이미 설정되어 있는지 확인합니다.-
@@ -25,4 +25,4 @@ fi
 ### Shell Execute Command
 # curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/set_proxy_server.sh | bash
 #
-# curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/set_proxy_server.sh | bash -s mirror.navercorp.com:3030 /tmp/bb
+# curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/set_proxy_server.sh | bash -s proxy_server:8080 /tmp/bb
