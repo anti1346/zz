61231284b90a9ae7282b1e39ea159b0aeadc84a4
Aby <42926329+anti1346@users.noreply.github.com>
2024-01-24 13:04:30 +0900

    Update set_nameserver.sh

--- etc/set_nameserver.sh
+++ etc/set_nameserver.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # 인수를 사용하여 첫 번째 DNS 서버를 설정하거나 기본값을 168.126.63.1로 설정합니다.
-variable1="${1:-168.126.63.1}"
+variable1="${1:-203.236.43.5}"
 
 # /etc/resolv.conf에 DNS 구성을 작성합니다.
 cat <<EOF > /etc/resolv.conf
 nameserver $variable1
+nameserver 168.126.63.1
 nameserver 8.8.8.8
 nameserver 8.8.4.4
 nameserver 1.1.1.1
@@ -14,3 +15,5 @@ EOF
 
 ### Shell Execute Command
 # curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/set_nameserver.sh | bash
+#
+# curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/set_nameserver.sh | bash -s 164.124.101.2
