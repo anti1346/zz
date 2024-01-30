700aab8bdf3f260e60f747e0f741b434134feba3
Aby <42926329+anti1346@users.noreply.github.com>
2024-01-30 10:14:45 +0900

    Update create_user.sh

--- etc/create_user.sh
+++ etc/create_user.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # 사용자 이름, 패스워드, uid, gid 설정
-USERNAME=${1:-vagrant}
-PASSWORD=${2:-vagrant}
+USER_NAME=${1:-vagrant}
+USER_PASSWORD=${2:-vagrant}
 USER_ID=${3:-2002}
 GROUP_ID=${4:-2002}
 KEYGEN=${5:-false} #true
 
 # 입력 유효성 검사
-if id "$USERNAME" &>/dev/null; then
-    echo "오류: '$USERNAME' 사용자가 이미 존재합니다." >&2
+if id "$USER_NAME" &>/dev/null; then
+    echo "오류: '$USER_NAME' 사용자가 이미 존재합니다." >&2
     exit 1
 fi
 
@@ -18,18 +18,18 @@ if [ "$USER_ID" -lt 1000 ]; then
 fi
 
 # 그룹 생성 및 사용자 추가
-groupadd -g "$GROUP_ID" "$USERNAME"
-useradd -m -c "deployment" -d "/home/$USERNAME" -s /bin/bash -u "$USER_ID" -g "$GROUP_ID" "$USERNAME"
+groupadd -g "$GROUP_ID" "$USER_NAME"
+useradd -m -c "deployment" -d "/home/$USER_NAME" -s /bin/bash -u "$USER_ID" -g "$GROUP_ID" "$USER_NAME"
 
 # 사용자 비밀번호 설정
-echo "$USERNAME:$PASSWORD" | chpasswd
+echo "$USER_NAME:$USER_PASSWORD" | chpasswd
 
 # sudoers 파일 설정
-# CentOS : echo -e '\n'$USERNAME'\tALL=(ALL)\tNOPASSWD: ALL' >> /etc/sudoers
-# Ubuntu : echo -e '\n'$USERNAME'\tALL=(ALL:ALL)\tNOPASSWD: ALL' >> /etc/sudoers
+# CentOS : echo -e '\n'$USER_NAME'\tALL=(ALL)\tNOPASSWD: ALL' >> /etc/sudoers
+# Ubuntu : echo -e '\n'$USER_NAME'\tALL=(ALL:ALL)\tNOPASSWD: ALL' >> /etc/sudoers
 if [ -x "$(command -v visudo)" ]; then
     echo "sudoers 파일을 수정 중입니다..."
-    echo "$USERNAME ALL=(ALL) NOPASSWD: ALL" | sudo visudo -f /etc/sudoers.d/"$USERNAME"
+    echo "$USER_NAME ALL=(ALL) NOPASSWD: ALL" | sudo visudo -f /etc/sudoers.d/"$USER_NAME"
     echo "sudoers 파일이 수정되었습니다."
 else
     echo "경고: visudo를 사용할 수 없습니다. sudoers 파일을 직접 수정하세요." >&2
@@ -37,7 +37,7 @@ fi
 
 # SSH 키 생성
 if [ "$KEYGEN" == "true" ]; then
-    su - $USERNAME <<EOF
+    su - $USER_NAME <<EOF
     ssh-keygen -t rsa -b 2048 -C "deployment"
 EOF
 else
