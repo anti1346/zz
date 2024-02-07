029b891b225156720ad0ded6eb06739bf34b73b8
anti1346 <anti1346@mt.co.kr>
2024-02-07 10:44:02 +0900

    no message

--- etc/docker_command.sh
+++ etc/docker_command.sh
@@ -2,7 +2,7 @@
 
 # 사용 방법을 출력하는 함수
 usage() {
-    echo "Usage: $0 {build|rm|rmi|inspect|run}" >&2
+    echo "Usage: $0 {build|buildx|rm|rmi|inspect|run}" >&2
     exit 1
 }
 
@@ -12,31 +12,35 @@ if [ -z "$1" ]; then
 fi
 
 # 컨테이너 이름과 이미지 이름을 변수로 지정
-image_name="anti1346/node-app:latest"
-container_name="node-app"
+docker_registry="anti1346"
+image_name="node-app"
+image_version="latest"
 
 # 입력된 명령어에 따라 동작을 결정
 case "$1" in
     build)
+        docker build --tag "$docker_registry/$image_name:$image_version" . --no-cache
+        ;;
+    buildx)
         # buildkitd 컨테이너가 없으면 buildx create 명령어 실행
         if ! docker ps | grep -q buildkitd; then
             docker buildx create --use
         fi
-        docker buildx build --tag $image_name . --push \
+        docker buildx build --tag "$docker_registry/$image_name:$image_version" . --push \
             --platform linux/amd64,linux/arm64 \
             --no-cache
         ;;
     rm)
-        docker rm -f $(docker ps -aq --filter="name=$container_name")
+        docker rm -f "$(docker ps -aq --filter="name=$image_name")"
         ;;
     rmi)
-        docker rmi -f $(docker images -aq --filter=reference='$image_name')
+        docker rmi -f "$(docker images -aq --filter=reference="$docker_registry/$image_name")"
         ;;
     inspect)
-        docker inspect $image_name --format='{{.Architecture}}'
+        docker inspect "$docker_registry/$image_name:$image_version" --format='{{.Architecture}}'
         ;;
     run)
-        docker run -d -p 3000:3000 --name $container_name --hostname $container_name $image_name
+        docker run -d -p 3000:3000 --name "$image_name" --hostname "$image_name" "$docker_registry/$image_name:$image_version"
         ;;
     *)
         echo "Invalid option: $1" >&2
