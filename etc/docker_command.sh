3e266a3e77afc0113b8c914536a125d6da1241ae
anti1346 <anti1346@mt.co.kr>
2024-02-07 09:58:47 +0900

    no message

--- etc/docker_command.sh
+++ etc/docker_command.sh
@@ -2,7 +2,7 @@
 
 # 사용 방법을 출력하는 함수
 usage() {
-    echo "Usage: $0 {ps|exec|build|buildx|pull|push|login|search|images|rm|rmi|system_prune|container_prune|image_prune|volume_prune|network_prune}" >&2
+    echo "Usage: $0 {build|rm|rmi|inspect|run}" >&2
     exit 1
 }
 
@@ -12,37 +12,19 @@ if [ -z "$1" ]; then
 fi
 
 # 컨테이너 이름과 이미지 이름을 변수로 지정
+image_name="anti1346/node-app:latest"
 container_name="node-app"
-image_name="anti1346/node-app"
 
 # 입력된 명령어에 따라 동작을 결정
 case "$1" in
-    ps)
-        docker ps
-        ;;
-    exec)
-        docker exec
-        ;;
     build)
-        docker build
-        ;;
-    buildx)
-        docker build buildx
-        ;;
-    pull)
-        docker pull
-        ;;
-    push)
-        docker push
-        ;;
-    login)
-        docker login
-        ;;
-    search)
-        docker search
-        ;;
-    images)
-        docker images
+        # buildkitd 컨테이너가 없으면 buildx create 명령어 실행
+        if ! docker ps | grep -q buildkitd; then
+            docker buildx create --use
+        fi
+        docker buildx build --tag $image_name . --push \
+            --platform linux/amd64,linux/arm64 \
+            --no-cache
         ;;
     rm)
         docker rm -f $(docker ps -aq --filter="name=$container_name")
@@ -50,26 +32,14 @@ case "$1" in
     rmi)
         docker rmi -f $(docker images -aq --filter=reference='$image_name')
         ;;
-    system_prune)
-        docker system prune
+    inspect)
+        docker inspect $image_name --format='{{.Architecture}}'
         ;;
-    container_prune)
-        docker container prune
-        ;;
-    image_prune)
-        docker image prune
-        ;;
-    volume_prune)
-        docker volume prune
-        ;;
-    network_prune)
-        docker network prune
+    run)
+        docker run -d -p 3000:3000 --name $container_name --hostname $container_name $image_name
         ;;
     *)
         echo "Invalid option: $1" >&2
         usage
         ;;
 esac
-
-### link
-# ln -s docker_command.sh /bin/dk
