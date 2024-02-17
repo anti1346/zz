10b48d0b85e92fda54d47cad3cd0c4363e4104ff
Aby <42926329+anti1346@users.noreply.github.com>
2024-02-17 18:08:16 +0900

    Create haproxy_sync_restart.sh

---
+++ etc/haproxy_sync_restart.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#############################
+# knode1 : target Server    #
+# knode2 : localhost Server #
+#############################
+
+haproxy_conf="/etc/haproxy/haproxy.cfg"
+target_server="root@knode1"
+
+# haproxy 실행 파일이 있는지 확인합니다.
+if command -v haproxy &>/dev/null; then
+    # haproxy.cfg 파일의 구문이 올바른지 확인합니다.
+    if haproxy -c -f "$haproxy_conf" -V; then
+        # localhost 서버의 haproxy를 다시 시작합니다.
+        sudo systemctl restart haproxy &&
+        
+        # 구문이 올바르다면 haproxy.cfg 파일을 Target 서버로 복사합니다.
+        scp "$haproxy_conf" "$target_server":"$haproxy_conf" &&
+        # Target 서버의 haproxy를 다시 시작합니다.
+        ssh "$target_server" "sudo systemctl restart haproxy" &&
+        
+        echo "HAProxy configuration synchronized and HAProxy restarted successfully."
+    else
+        echo "ERROR: Configuration file is not valid. Please check haproxy.cfg."
+    fi
+else
+    echo "ERROR: haproxy command not found. Please install haproxy."
+fi
