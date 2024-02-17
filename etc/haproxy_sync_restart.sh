8d0530d5e13e7136535fe2e63e8da7c1308914df
Aby <42926329+anti1346@users.noreply.github.com>
2024-02-17 18:21:11 +0900

    Update haproxy_sync_restart.sh

--- etc/haproxy_sync_restart.sh
+++ etc/haproxy_sync_restart.sh
@@ -11,13 +11,17 @@ target_server="root@knode1"
 if command -v haproxy &>/dev/null; then
     # haproxy.cfg 파일의 구문이 올바른지 확인합니다.
     if haproxy -c -f "$haproxy_conf" -V; then
+        echo "haproxy configuration syntax is valid. Proceeding..."
+
         # localhost 서버의 haproxy를 다시 시작합니다.
+        echo "Restarting HAProxy on target server..."
         sudo systemctl restart haproxy &&
         
         # 구문이 올바르다면 haproxy.cfg 파일을 Target 서버로 복사합니다.
-        scp "$haproxy_conf" "$target_server":"$haproxy_conf" &&
+        scp -q "$haproxy_conf" "$target_server":"$haproxy_conf"
         # Target 서버의 haproxy를 다시 시작합니다.
-        ssh "$target_server" "sudo systemctl restart haproxy" &&
+        echo -e "\nRestarting HAProxy on target server..."
+        ssh "$target_server" "sudo systemctl restart haproxy"
         
         echo "HAProxy configuration synchronized and HAProxy restarted successfully."
     else
