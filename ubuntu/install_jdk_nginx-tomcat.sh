6673a6847e3e6d89c014e1c0155851367893e5eb
Aby <42926329+anti1346@users.noreply.github.com>
2024-05-19 15:09:42 +0900

    Update install_jdk_nginx-tomcat.sh

--- ubuntu/install_jdk_nginx-tomcat.sh
+++ ubuntu/install_jdk_nginx-tomcat.sh
@@ -6,9 +6,12 @@ sudo wget -q https://download.oracle.com/java/17/archive/jdk-17.0.10_linux-aarch
 sudo tar -xzf /app//app/jdk-17.0.10_linux-aarch64_bin.tar.gz -C /app/java --strip-components=1
 
 # JDK 환경 변수 설정
-echo 'export JAVA_HOME=/app/java' | sudo tee /etc/profile.d/jdk.sh
-echo 'export PATH=$PATH:$JAVA_HOME/bin' | sudo tee -a /etc/profile.d/jdk.sh
-source /etc/profile.d/jdk.sh
+# JDK 환경 변수 설정
+if [ ! -f /etc/profile.d/jdk.sh ] || ! grep -q 'export JAVA_HOME=/app/java' /etc/profile.d/jdk.sh; then
+    echo 'export JAVA_HOME=/app/java' | sudo tee /etc/profile.d/jdk.sh
+    echo 'export PATH=$PATH:$JAVA_HOME/bin' | sudo tee -a /etc/profile.d/jdk.sh
+    source /etc/profile.d/jdk.sh
+fi
 
 # NGINX 설치
 sudo apt-get update
@@ -22,15 +25,15 @@ sudo systemctl --now enable nginx
 # Tomcat 설치 및 설정
 sudo groupadd tomcat
 sudo useradd -s /bin/false -g tomcat -d /app/tomcat tomcat
-sudo mkdir -p /app/tomcat/{instance1,instance2}
+sudo mkdir -p /app/tomcat/{tomcat1,tomcat2}
 sudo wget -q https://downloads.apache.org/tomcat/tomcat-9/v9.0.89/bin/apache-tomcat-9.0.89.tar.gz -O /app/apache-tomcat-9.tar.gz
-sudo tar -xzf /app/apache-tomcat-9.0.89.tar.gz -C /app/tomcat/instance1 --strip-components=1
-sudo tar -xzf /app/apache-tomcat-9.0.89.tar.gz -C /app/tomcat/instance2 --strip-components=1
+sudo tar -xzf /app/apache-tomcat-9.0.89.tar.gz -C /app/tomcat/tomcat1 --strip-components=1
+sudo tar -xzf /app/apache-tomcat-9.0.89.tar.gz -C /app/tomcat/tomcat2 --strip-components=1
 
 # 인스턴스의 server.xml 파일 수정
 TOMCAT_HOME="/app/tomcat"
-INSTANCE1_PATH="instance1"
-INSTANCE2_PATH="instance2"
+INSTANCE1_NAME="tomcat1"
+INSTANCE2_NAME="tomcat2"
 
 INSTANCE1_SHUTDOWN_PORT="8001"
 INSTANCE1_CONNECTOR_PORT="8081"
@@ -41,17 +44,17 @@ INSTANCE2_REDIRECT_PORT="8542"
 # 인스턴스 1의 server.xml 파일 수정
 sed -i "s/port=\"8005\"/port=\"$INSTANCE1_SHUTDOWN_PORT\"/g; \
         s/port=\"8080\"/port=\"$INSTANCE1_CONNECTOR_PORT\"/g; \
-        s/redirectPort=\"8443\"/redirectPort=\"$INSTANCE1_REDIRECT_PORT\"/g" "$TOMCAT_HOME/$INSTANCE1_PATH/conf/server.xml"
+        s/redirectPort=\"8443\"/redirectPort=\"$INSTANCE1_REDIRECT_PORT\"/g" "$TOMCAT_HOME/$INSTANCE1_NAME/conf/server.xml"
 # 인스턴스 2의 server.xml 파일 수정
 sed -i "s/port=\"8005\"/port=\"$INSTANCE2_SHUTDOWN_PORT\"/g; \
         s/port=\"8080\"/port=\"$INSTANCE2_CONNECTOR_PORT\"/g; \
-        s/redirectPort=\"8443\"/redirectPort=\"$INSTANCE2_REDIRECT_PORT\"/g" "$TOMCAT_HOME/$INSTANCE2_PATH/conf/server.xml"
+        s/redirectPort=\"8443\"/redirectPort=\"$INSTANCE2_REDIRECT_PORT\"/g" "$TOMCAT_HOME/$INSTANCE2_NAME/conf/server.xml"
 
 sudo chown -R tomcat:tomcat /app/tomcat
 
 # Tomcat 서비스 파일 작성
 for instance in tomcat1 tomcat2; do
-    cat <<EOF | sudo tee /etc/systemd/system/$instance.service >/dev/null
+    cat <<EOF | sudo tee "/etc/systemd/system/$instance.service" >/dev/null
 [Unit]
 Description=Tomcat Instance $instance
 After=network.target
