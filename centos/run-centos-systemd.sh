67638d664056875d5806c954c63c415b81e8a14f
Aby <42926329+anti1346@users.noreply.github.com>
2024-05-19 21:20:53 +0900

    Create run-centos-systemd.sh

---
+++ centos/run-centos-systemd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run -d \
+--privileged \                                      # 컨테이너에 특권 모드 부여
+-p 80:80 \                                          # HTTP 포트 포워딩
+-p 443:443 \                                        # HTTPS 포트 포워딩
+-p 2222:22 \                                        # SSH 포트 포워딩
+-v /etc/localtime:/usr/share/zoneinfo/Asia/Seoul \  # 호스트와 컨테이너의 시간 동기화
+--name ubuntu-systemd \                             # 컨테이너 이름 지정
+anti1346/centos7:systemd                            # 이미지와 태그 지정
