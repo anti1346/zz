167a8317127d39dc732f60a4948ba7d4aea1337d
anti1346 <anti1346@mt.co.kr>
2024-05-21 09:44:34 +0900

    no message

--- common/install_ansible.sh
+++ common/install_ansible.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-# Check if Ansible is already installed
+# Ansible이 이미 설치되어 있는지 확인합니다.
 if [ -x "$(command -v ansible)" ]; then
   echo "Ansible is already installed. Exiting..."
   exit 0
 fi
 
-# 우분투 패키지 업데이트
-sudo apt-get update
-
-# 소프트웨어 소스 관리 도구 및 HTTPS 지원 패키지 설치
-sudo apt-get install -y software-properties-common apt-transport-https
-
-# Ansible 공식 저장소 추가 및 키 가져오기
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-
-# 최신 버전의 Ansible 설치
-sudo apt-get install -y ansible
-
-# Ansible 버전 확인
-sudo echo -e "Ansible 버전:\n\n `ansible --version`"
+# 우분투에 Ansible을 설치합니다.
+if command -v apt-get &> /dev/null; then
+  # 패키지 목록 업데이트
+  sudo apt-get update
+  # 소프트웨어 소스 관리 도구 및 HTTPS 지원 패키지 설치
+  sudo apt-get install -y software-properties-common apt-transport-https
+  # Ansible의 공식 PPA 추가 및 패키지 목록 업데이트
+  sudo apt-add-repository --yes --update ppa:ansible/ansible
+  # Ansible 설치
+  sudo apt-get install -y ansible
+  # Ansible 버전 확인
+  echo -e "Ansible 버전:\n\n$(ansible --version)"
+else
+  echo "Unsupported package manager. This script supports only systems with apt-get."
+  exit 1
+fi
