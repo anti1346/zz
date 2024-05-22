139abe6493d808cb48d5f6ac75d34d6c2e5a5041
anti1346 <anti1346@mt.co.kr>
2024-05-22 14:39:09 +0900

    no message

--- ubuntu/install_mysql8.sh
+++ ubuntu/install_mysql8.sh
@@ -42,7 +42,7 @@ sudo chown -R mysql:mysql ${MYSQL_INSTALL_DIR}
 
 # MySQL 환경 변수 등록
 if ! grep -q "${MYSQL_INSTALL_DIR}/bin" ~/.bashrc; then
-    echo -e '\nexport PATH=${MYSQL_INSTALL_DIR}/bin:$PATH' >> ~/.bashrc
+    echo -e "\nexport PATH=${MYSQL_INSTALL_DIR}/bin:\$PATH" >> ~/.bashrc
     source ~/.bashrc
 fi
 
