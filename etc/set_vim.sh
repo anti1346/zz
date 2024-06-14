3a144ab08ff54cbb5644fa6d18a4cafaed3216f5
anti1346 <anti1346@mt.co.kr>
2024-06-14 11:06:27 +0900

    no message

---
+++ etc/set_vim.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cat << EOF > ~/.vimrc
+set encoding=utf-8
+set fileencoding=utf-8
+set termencoding=utf-8
+EOF
+
+### Shell Execute Command
+# curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/set_vim.sh | bash
