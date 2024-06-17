b79f80eccc9a394fa8b463546137d71f2730caad
anti1346 <anti1346@mt.co.kr>
2024-06-17 12:43:15 +0900

    no message

--- etc/default_package_install_on_docker_container.sh
+++ etc/default_package_install_on_docker_container.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
 
+# 함수 정의
+set_vim() {
+    if ! command -v dos2unix &> /dev/null; then
+        echo "dos2unix is not installed. Please install it first."
+        exit 1
+    fi
+
+    if curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/set_vim.sh | dos2unix | bash; then
+        echo "Vim configuration script executed successfully."
+    else
+        echo "Failed to download or execute the Vim configuration script."
+        exit 1
+    fi
+}
+
 # Determine the package manager
 if [[ "$(command -v yum)" ]]; then
     PACKAGE_MANAGER="yum"
+
+    sudo $PACKAGE_MANAGER install -y vim
+    set_vim
 elif [[ "$(command -v apt-get)" ]]; then
     PACKAGE_MANAGER="apt-get"
-    $PACKAGE_MANAGER update
+
+    sudo $PACKAGE_MANAGER update
+    sudo $PACKAGE_MANAGER install -y vim dos2unix
+    sudo update-alternatives --set editor /usr/bin/vim.basic
+    set_vim
 else
     echo "Unsupported package manager."
     exit 1
 fi
-
 
 
 
@@ -19,8 +40,3 @@ fi
 # 
 # apt-get install -y dos2unix
 # curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/default_package_install_on_docker_container.sh | dos2unix | bash
-
-
-
-### Setting up vim in Ubuntu
-# sudo update-alternatives --set editor /usr/bin/vim.basic
