928f61b782ae62ac3fd82be8d818e5a81188022f
anti1346 <anti1346@mt.co.kr>
2024-06-17 10:40:53 +0900

    no message

--- etc/set_vim.sh
+++ etc/set_vim.sh
@@ -6,6 +6,15 @@ set fileencoding=utf-8
 set termencoding=utf-8
 EOF
 
+
+
 ### Shell Execute Command
 # curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/set_vim.sh | bash
+# 
+# apt-get install -y dos2unix
 # curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/set_vim.sh | dos2unix | bash
+
+
+
+### Setting up vim in Ubuntu
+# sudo update-alternatives --set editor /usr/bin/vim.basic
