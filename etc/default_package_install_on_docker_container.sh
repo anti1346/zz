90ca3e9feb153e85c152b7524b2084dac340f558
anti1346 <anti1346@mt.co.kr>
2024-06-17 12:37:34 +0900

    no message

--- etc/default_package_install_on_docker_container.sh
+++ etc/default_package_install_on_docker_container.sh
@@ -5,6 +5,7 @@ if [[ "$(command -v yum)" ]]; then
     PACKAGE_MANAGER="yum"
 elif [[ "$(command -v apt-get)" ]]; then
     PACKAGE_MANAGER="apt-get"
+    $PACKAGE_MANAGER update
 else
     echo "Unsupported package manager."
     exit 1
@@ -14,10 +15,10 @@ fi
 
 
 ### Shell Execute Command
-# curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/set_vim.sh | bash
+# curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/default_package_install_on_docker_container.sh | bash
 # 
 # apt-get install -y dos2unix
-# curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/set_vim.sh | dos2unix | bash
+# curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/default_package_install_on_docker_container.sh | dos2unix | bash
 
 
 
