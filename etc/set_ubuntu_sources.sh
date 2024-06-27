dc6aa457f4ad8995937ae2accdf93355fd6d6e17
anti1346 <anti1346@mt.co.kr>
2024-06-27 12:47:59 +0900

    no message

--- etc/set_ubuntu_sources.sh
+++ etc/set_ubuntu_sources.sh
@@ -8,8 +8,10 @@ else
     mirror_server="$1"
 fi
 
+# 기존 소스 목록 파일 백업
+sudo cp /etc/apt/sources.list /etc/apt/sources.list-$(date '+%Y%m%d_%H%M%S')
+
 # mirror.kakao.com을 사용하도록 패키지 소스를 업데이트합니다.
-#sudo sed -i "s/\(kr\|archive\|ports\).ubuntu.com/$mirror_server/g" /etc/apt/sources.list
 sudo sed -i.bak "s/\(kr\|archive\|ports\).ubuntu.com/$mirror_server/g" /etc/apt/sources.list
 
 # 패키지 목록 업데이트
