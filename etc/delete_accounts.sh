b033375783aaa8f86fe244f6d5cfcf2d69e13438
Aby <42926329+anti1346@users.noreply.github.com>
2024-07-15 20:19:03 +0900

    Update delete_accounts.sh

--- etc/delete_accounts.sh
+++ etc/delete_accounts.sh
@@ -6,8 +6,8 @@
 # cut -d: -f1 /etc/group
 
 # 삭제할 사용자 및 그룹 목록
-USERS_TO_DELETE=("adm" "lp" "sync" "shutdown" "halt" "mail" "operator" "games" "ftp" "nfsnobody")
-GROUPS_TO_DELETE=("cdrom" "floppy" "games" "audio")
+USERS_TO_DELETE=("adm" "lp" "sync" "shutdown" "halt" "mail" "operator" "games" "ftp" "nfsnobody" "uucp" "news")
+GROUPS_TO_DELETE=("cdrom" "floppy" "games" "audio" "dialout" "fax" "voice" "tape")
 
 # 사용자 삭제
 for USER in "${USERS_TO_DELETE[@]}"; do
@@ -32,4 +32,7 @@ done
 echo "Script execution completed."
 
 ### 실행 권한 여부
+# curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/delete_accounts.sh -o delete_accounts.sh
 # chmod +x delete_accounts.sh
+
+curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/delete_accounts.sh | dos2unix | bash
