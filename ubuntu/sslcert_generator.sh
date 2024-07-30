ba18f09dbea6f8ac837caf17342972e98d15abb3
aby <anti1346@gmail.com>
2024-07-30 20:40:30 +0900

    no message

--- ubuntu/sslcert_generator.sh
+++ ubuntu/sslcert_generator.sh
@@ -62,3 +62,4 @@ openssl x509 -req -in $SSLDIR/peer.csr -CA $SSLDIR/ca.crt -CAkey $SSLDIR/ca.key 
 ### Shell Execute Command
 # curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/ubuntu/sslcert_generator.sh -o sslcert_generator.sh
 # chmod -x sslcert_generator.sh
+
