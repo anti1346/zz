62da4f30e7c038df0ff6e4d4969e1bc282c8bf82
Aby <42926329+anti1346@users.noreply.github.com>
2024-07-30 10:18:44 +0900

    Create install_cfssl.sh

---
+++ ubuntu/install_cfssl.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 필요한 명령어 확인
+sudo apt-get update
+command -v curl >/dev/null 2>&1 || { echo >&2 "curl이 설치되어 있지 않습니다."; sudo apt-get install -y curl; }
+command -v jq >/dev/null 2>&1 || { echo >&2 "jq가 설치되어 있지 않습니다."; sudo apt-get install -y jq; }
+
+# 최신 cfssl 버전 가져오기
+LATEST_VERSION=$(curl -sfSL "https://api.github.com/repos/cloudflare/cfssl/releases/latest" | jq -r '.tag_name' | sed 's/^v//')
+if [ -z "$LATEST_VERSION" ]; then
+    echo "최신 버전 정보를 가져오는 데 실패했습니다."
+    exit 1
+fi
+
+# cfssl 다운로드 및 설치
+curl -sfSL https://github.com/cloudflare/cfssl/releases/download/v${LATEST_VERSION}/cfssl_${LATEST_VERSION}_linux_amd64 -o /usr/local/bin/cfssl
+chmod +x /usr/local/bin/cfssl
+
+# cfssljson 다운로드 및 설치
+curl -sfSL https://github.com/cloudflare/cfssl/releases/download/v${LATEST_VERSION}/cfssljson_${LATEST_VERSION}_linux_amd64 -o /usr/local/bin/cfssljson
+chmod +x /usr/local/bin/cfssljson
+
+# 버전 확인
+cfssl version
+cfssljson --version
