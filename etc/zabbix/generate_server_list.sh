5bc2b5f7c0a50cb856afa132b215d30978ea1cc4
Aby <42926329+anti1346@users.noreply.github.com>
2025-02-25 21:25:25 +0900

    Update generate_server_list.sh

--- etc/zabbix/generate_server_list.sh
+++ etc/zabbix/generate_server_list.sh
@@ -5,21 +5,23 @@ DB_USER="zabbix"
 DB_PASS="비밀번호"
 DB_NAME="zabbix"
 
-# server_list 배열 초기화
-server_list=()
+# 서버 목록 배열 선언
+declare -a server_list=()
 
-# SQL 실행 후 결과를 배열에 저장
+# MySQL에서 IP와 호스트명을 가져와 배열에 저장
 while IFS= read -r line; do
     server_list+=("\"$line\"")
-done < <(mysql -u "$DB_USER" -p"$DB_PASS" -D "$DB_NAME" -N -e "
-SELECT CONCAT(i.ip, ' ', h.host)
-FROM hosts h
-JOIN interface i ON h.hostid = i.hostid
-WHERE h.status = 0 AND i.type = 1;
-")
+done < <(
+    mysql -u "$DB_USER" -p"$DB_PASS" -D "$DB_NAME" -N -e "
+        SELECT CONCAT(i.ip, ' ', h.host)
+        FROM hosts h
+        JOIN interface i ON h.hostid = i.hostid
+        WHERE h.status = 0 AND i.type = 1;
+    "
+)
 
 # 배열 출력
-echo "server_list=("
+echo "declare -a server_list=("
 for entry in "${server_list[@]}"; do
     echo "    $entry"
 done
